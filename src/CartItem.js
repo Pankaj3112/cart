d3f55cfe418c71d98b5873df67950dbd20569d9b
Pankaj <pankajbeniwal3112@gmail.com>
2023-09-18 13:50:03 +0530

    modify state

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,45 @@
 import React from "react";
 
 class CartItem extends React.Component{
+
+    constructor(){
+        super();
+        this.state = {
+            price: 999,
+            title: 'Mobile Phone',
+            qty: 1,
+            img: ''
+        }
+    }
+
+    increaseQuantity = () =>{
+        //form 1 of set state
+        // this.setState({
+        //     qty: this.state.qty+1,
+        // })
+
+        //form 2 of set state - if prev state required
+        this.setState((prevState) => {
+           return {
+                qty: prevState.qty+1,
+           }
+        })
+    }
+
+    decreaseQuantity = () =>{
+        this.setState((prevState) => {
+            return {
+                qty: prevState.qty - 1
+            }
+        })
+    }
+
+
+
     render(){
+
+        const {price, title, qty} = this.state;
+
         return(
             <div className="cart-item">
                 <div className="left-block">
@@ -9,13 +47,25 @@ class CartItem extends React.Component{
                 </div>
 
                 <div className="right-block">
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{color: '#777'}}>Rs 999</div>
-                    <div style={{color: '#777'}}>Qty: 1</div>
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{color: '#777'}}>Rs {price}</div>
+                    <div style={{color: '#777'}}>Qty: {qty}</div>
                 </div>
 
                 <div className="cart-item-actions">
                     {/* Buttons */}
+                    <span className="action-icons" onClick={this.increaseQuantity}>
+                        <i className="fa-solid fa-plus"></i>
+                    </span>
+
+                    <span className="action-icons" onClick={this.decreaseQuantity}>
+                        <i className="fa-solid fa-minus"></i>
+                    </span>
+
+                    <span className="action-icons" onClick={this.increaseQuantity}>
+                        <i className="fa-solid fa-trash"></i>
+                    </span>
+                    
                 </div>
             </div>
         );
